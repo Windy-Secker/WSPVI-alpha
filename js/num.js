d155edecdbb7da585b9eb6b2353a55ca980ce248
Windy-Secker <75884241+Windy-Secker@users.noreply.github.com>
2021-01-09 01:33:35 -0600

    Update num.js

--- js/num.js
+++ js/num.js
@@ -2,5 +2,5 @@ var ver;
 var cop;
 ver="0.1.1";
 cop="2021";
-document.getElementsById("version").innerHtml="<a href=/README.md>" & ver & "</a>";
-document.getElementsById("copyear").innerHtml="<a href=/README.md>" & cop & "</a>";
+document.getElementById("version").innerHtml=ver;
+document.getElementById("copyear").innerHtml=cop;
