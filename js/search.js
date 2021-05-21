40b35a8b4a9958b22c7b7d673d9a8a500cb6df0e
Windy-Secker <75884241+Windy-Secker@users.noreply.github.com>
2021-05-21 19:13:38 +0800

    Create search.js

---
+++ js/search.js
@@ -0,0 +1,28 @@
+
+function s_1()
+
+{window.open("http://www.google.com/search?hl=zh-CN&q="+encodeURI(f_g1.key.value));return false;};
+
+function s_2()
+
+{window.open("http://www.baidu.com/baidu?tn=gg5g&word="+encodeURI(f_g2.key.value));return false;};
+
+function s_3()
+
+{window.open("http://www.sogou.com/web?query="+encodeURI(f_g3.key.value));return false;};
+
+function s_4()
+
+{window.open("http://www.soku.com/v?keyword="+encodeURI(f_g4.key.value));return false;};
+
+function s_5()
+
+{window.open("http://cn.bing.com/search?q="+encodeURI(f_g5.key.value));return false;};
+
+function s_6()
+
+{window.open("http://image.baidu.com/i?ct=201326592&&word="+encodeURI(f_g6.key.value));return false;};
+
+function s_7()
+
+{window.open("https://yandex.com/search/?text="+encodeURI(f_g7.key.value));return false;};
